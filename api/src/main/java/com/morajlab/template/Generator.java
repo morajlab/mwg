08d1392a3579124f499c49ffbba62fc1774466d2
Morteza Jamali <mortezajamali4241@gmail.com>
2021-12-11 08:37:47 +0000

    feat(api): create 'Generator' java class

---
+++ api/src/main/java/com/morajlab/template/Generator.java
@@ -0,0 +1,11 @@
+package com.morajlab.template;
+
+public class Generator {
+  Generator() {
+    // Cunstructure code
+  }
+
+  public void generate() {
+    System.out.println("This is `generate()` method");
+  }
+}
