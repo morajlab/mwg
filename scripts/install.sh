2ee2aa6824f748f4183b2a791a52872c50f1e9f3
Morteza Jamali <mortezajamali4241@gmail.com>
2022-08-04 18:04:33 +0000

    fix(packages): use log `module` and fix `generate` command

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,9 +16,21 @@ EOF
   rm -rf "$TMP_PATH"
 }
 
+install_bpm() {
+  curl -fsSL \
+  https://raw.githubusercontent.com/morajlab/bpm/master/packages/install/install.sh | bash
+}
+
 if ! command -v esh &> /dev/null; then
   (install_esh)
 fi
+
+if ! command -v bpm &> /dev/null; then
+  (install_bpm)
+  source $HOME/.bashrc
+fi
+
+bpm install log color
 
 npm i
 
